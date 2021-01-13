074f9197cfb1e0541b3027ab5895e4a9e530f771
David Aronchick <aronchick@gmail.com>
2021-01-13 22:46:20 +0000

    updating location of main.go

--- go.mod
+++ go.mod
@@ -5,13 +5,15 @@ go 1.14
 require (
 	github.com/Azure/azure-sdk-for-go v50.0.0+incompatible
 	github.com/Azure/go-autorest/autorest v0.11.15 // indirect
-	github.com/Azure/go-autorest/autorest/azure/auth v0.5.5
+	github.com/Azure/go-autorest/autorest/adal v0.9.8 // indirect
 	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
 	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
+	github.com/onrik/logrus v0.8.0
 	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/tidwall/gjson v1.6.7 // indirect
 	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
---
+++ set_env_vars_sample.sh
@@ -0,0 +1,21 @@
+export SAME_SUBSCRIPTION_ID='XXXXXXXXXXXXXXXXXX'
+az account set --subscription $SAME_SUBSCRIPTION_ID
+export SAME_CLUSTER_NAME='XXXXXXXXXXXXXXXXXX'
+export SAME_CLUSTER_RG='XXXXXXXXXXXXXXXXXX'
+export SAME_CLUSTER_VERSION=`az aks show -n $SAME_CLUSTER_NAME -g $SAME_CLUSTER_RG -o json | jq -r '.kubernetesVersion'`
+az aks get-credentials -n $SAME_CLUSTER_NAME -g $SAME_CLUSTER_RG
+export SAME_SP_NAME="same_service_principal_$(whoami)"
+export SAME_SP_JSON=`az ad sp create-for-rbac --scope "/subscriptions/$SAME_SUBSCRIPTION_ID/resourceGroups/$SAME_CLUSTER_RG" --role Contributor --sdk-auth`
+
+export AZURE_ENVIRONMENT="AzurePublicCloud"
+
+export AZURE_TENANT_ID_B64=$(az account show --subscription=$SAME_SUBSCRIPTION_ID -o json | jq -r '.tenantId' | base64 | tr -d '\n')
+export AZURE_CLIENT_ID=$(echo -n $SAME_SP_JSON | jq -r '.clientId')
+export AZURE_CLIENT_ID_B64=$(echo -n $AZURE_CLIENT_ID | base64 | tr -d '\n')
+export AZURE_CLIENT_SECRET=$(echo -n $SAME_SP_JSON | jq -r '.clientSecret')
+export AZURE_CLIENT_SECRET_B64=$(echo -n $AZURE_CLIENT_SECRET | base64 | tr -d '\n')
+export AZURE_SUBSCRIPTION_ID=$(echo -n $SAME_SUBSCRIPTION_ID | base64 | tr -d '\n')
+export AZURE_SUBSCRIPTION_ID_B64=$(echo -n $SAME_SUBSCRIPTION_ID | base64 | tr -d '\n')
+export AZURE_LOCATION="centralus"
+export AZURE_CONTROL_PLANE_MACHINE_TYPE="Standard_D2s_v3"
+export AZURE_NODE_MACHINE_TYPE="Standard_D2s_v3"
