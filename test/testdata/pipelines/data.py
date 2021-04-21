18fc97ce9465a86f25bf0f33e1918c0c23aac57d
Bernd Verst <berndverst@users.noreply.github.com>
2021-04-21 14:15:23 -0700

    Remove PVC from Storage Account from test pipeline (#224)

--- test/testdata/pipelines/data.py
+++ test/testdata/pipelines/data.py
@@ -1,18 +1,16 @@
-def prepare_dataset(mnt_path: str = '/mnt/azure'):
+from kfp.components import OutputPath
+
+def prepare_dataset(output_path: OutputPath(str)):
 
     import requests
     from pathlib import Path
 
-    base_path = Path(mnt_path).resolve(strict=False)
-    print('Base Path:  {}'.format(base_path))
-    data_path = base_path.joinpath('iris_training.csv').resolve(strict=False)
-
     # Load raw data
     train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
-    print('Downloading dataset from {0} to {1}'.format(train_dataset_url, str(data_path)))
+    print('Downloading dataset from {0} to {1}'.format(train_dataset_url, output_path))
     data = requests.get(train_dataset_url).content
 
-    with open(str(data_path), 'wb') as writer:
+    with open(output_path, 'wb') as writer:
         writer.write(data)
     
     print('Done.')
